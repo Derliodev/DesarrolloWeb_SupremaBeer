3f382f89394fa064444268418bb2ccb0e3926f5d
Leonardo Dev <derliodev@gmail.com>
2022-03-02 18:25:03 -0300

    Archivos finales del desarrollo del sitio web

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+function esconder(){
+    document.getElementById("login").style.display = "none";
+}
+function abrir(){
+    document.getElementById("login").style.display = "block";
+}
+function enviar(){
+    alert("Se ha mandado su mensaje,\nen breves instantes nos contactaremos");
+    location.href="index.html";
+}
+/* Ale */
+function cale(){
+    document.getElementById("ale").style.display = "none";
+}
+function aale(){
+    document.getElementById("ale").style.display = "block";
+}
+/* Lager */
+function clager(){
+    document.getElementById("lager").style.display = "none";
+}
+function alager(){
+    document.getElementById("lager").style.display = "block";
+}
